92f2f89110dff9399e692c4df6db4fbf6082ed32
Ahmed Abbas <tgahmed110@gmail.com>
2026-09-21 22:57:18 +0500

    fix: pass Clerk publishable key to middleware

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+﻿import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({ publishableKey: process.env.CLERK_PUBLISHABLE_KEY });
 
 export const config = {
   matcher: [
